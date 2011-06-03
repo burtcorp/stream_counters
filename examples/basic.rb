00b60e45b998da4b1482f0d7c5ae0f7f4dc9db69
Theo <theo@iconara.net>
2011-06-03 20:56:21 +0200

    Basic example

---
+++ examples/basic.rb
@@ -0,0 +1,48 @@
+$: << File.expand_path('../../lib', __FILE__)
+
+require 'stream_counters'
+
+class Visit
+  attr_reader :api_key, :date, :path
+  
+  def initialize(data={})
+    @api_key, @date, @path, @click = data.values_at(:api_key, :date, :path, :click)
+  end
+  
+  def section
+    if @path == '/'
+    then 'homepage'
+    else @path.split('/')[1]
+    end
+  end
+  
+  def pageviews
+    1
+  end
+  
+  def click?
+    @click
+  end
+end
+
+include StreamCounters::ConfigurationDsl
+
+conf = configuration do
+  main_keys :api_key, :date
+  dimension :path
+  dimension :section
+  metric :pageviews
+  metric :clicks, :click?, :type => :predicate
+end
+
+counters = conf.create_counters(:filters => {:predicate => lambda { |v| v ? 1 : 0 }})
+counters.count(Visit.new(:api_key => 'ABC', :date => Time.utc(2011, 6, 3), :path => '/blog/article2',       :click => false))
+counters.count(Visit.new(:api_key => 'ABC', :date => Time.utc(2011, 6, 3), :path => '/jobs/monkey-handler', :click => false))
+counters.count(Visit.new(:api_key => 'DEF', :date => Time.utc(2011, 6, 4), :path => '/',                    :click => false))
+counters.count(Visit.new(:api_key => 'DEF', :date => Time.utc(2011, 6, 4), :path => '/blog/article1',       :click => false))
+counters.count(Visit.new(:api_key => 'ABC', :date => Time.utc(2011, 6, 4), :path => '/qa/page3',            :click => false))
+counters.count(Visit.new(:api_key => 'GHI', :date => Time.utc(2011, 6, 4), :path => '/jobs/sommelier',      :click => true))
+counters.count(Visit.new(:api_key => 'ABC', :date => Time.utc(2011, 6, 5), :path => '/',                    :click => true))
+counters.each do |data|
+  p data
+end

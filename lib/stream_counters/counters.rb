ad9af13d2dac1a739dbab3782dbca663e010b369
Daniel Gaiottino <daniel.gaiottino@gmail.com>
2011-06-10 16:43:45 +0200

    Refactored how Specials work

--- lib/stream_counters/counters.rb
+++ lib/stream_counters/counters.rb
@@ -15,35 +15,44 @@ module StreamCounters
         segment_values = dimension.all_keys.map { |dim| item.send(dim) }
         counters_for_key = (@counters[keys] ||= {})
         counters_for_dim = (counters_for_key[dimension] ||= {})
-        counters_for_seg = (counters_for_dim[segment_values] ||= default_counters(dimension))
+        counters_for_seg = (counters_for_dim[segment_values] ||= @metrics_counters[dimension].dup)
         dimension.metrics.each do |metric_name, metric|
           counters_for_seg[metric_name] = reduce(counters_for_seg[metric_name], item, metric)
         end
-        @specials.each do |special|
-          special.calculate(counters_for_seg, item)
+        
+        if @specials.any?
+          specials_for_key = (@special_counters[keys] ||= {})
+          specials_for_dim = (specials_for_key[dimension] ||= {})
+          specials_for_seg = (specials_for_dim[segment_values] ||= @specials.map { |special| special.new })
+
+          specials_for_seg.each do |special|
+            special.count(item)
+          end
         end
       end
     end
     
     def reset
       @counters = {}
+      @special_counters = {}
       @metrics_counters = @config.dimensions.reduce({}) do |acc, dimension|
         acc[dimension] = Hash[dimension.metrics.map { |name, metric| [name, metric.default] }].freeze
         acc
       end
+      @special_counters.each { |special| special.reset }
     end
     
     def get(keys, dimension)
-      @counters[keys][dimension]
+      merge_specials(@counters, keys, dimension)
     end
     
     def each(&block)
       @counters.keys.each do |keys|
         counters_for_keys = @counters[keys]
         counters_for_keys.keys.each do |dimension|
-          counters_for_dims = counters_for_keys[dimension]
-          counters_for_dims.keys.each do |segment|
-            data = Hash[@config.base_keys.zip(keys) + dimension.all_keys.zip(segment)].merge!(counters_for_dims[segment])
+          counters_for_dim = merge_specials(@counters, keys, dimension)
+          counters_for_dim.keys.each do |segment|
+            data = Hash[@config.base_keys.zip(keys) + dimension.all_keys.zip(segment)].merge!(counters_for_dim[segment])
             case block.arity
             when 1 then yield data
             else        yield data, dimension
@@ -70,10 +79,18 @@ module StreamCounters
     
   private
   
-    def default_counters(dimension)
-      @specials.reduce(@metrics_counters[dimension].dup) do |counters, special|
-        counters.merge!(special.default)
+    def merge_specials(counters, keys, dimension)
+      counters_for_dim = counters[keys][dimension]
+      if @specials.any?
+        @special_counters[keys][dimension].reduce(counters_for_dim) do |counters, specials_for_dim|
+          segment, specials_for_seg = specials_for_dim
+          specials_for_seg.each do |special|
+            counters[segment].merge!(special.value)
+          end
+          counters
+        end
       end
+      counters_for_dim
     end
   end
 end
